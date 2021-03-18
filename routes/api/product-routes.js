12a4124c1f4a0b7dbb9de37528b408989ff4f2c5
Cody Kimb <codykimbell@gmail.com>
2021-03-18 17:25:08 -0500

    category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -62,12 +62,6 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
- Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: stock,
-    tagIds: req.body.category_id
- })
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
